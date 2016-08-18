8aa4093cf73b17de2725d53cbc9b441c55e4734e
just-fy <just-fy@mail.ru>
2016-08-19 00:35:16 +0300

    скрипт для добавления в закладки

---
+++ js/favorite.js
@@ -0,0 +1,6 @@
+/**
+ * Created by user on 19.08.2016.
+ */
+function addBookmark() {
+    if (document.all) window.external.addFavorite('file:///D:/Nastya/работа/pokupo/InfoSkills.git/index.html', 'Сайт infoskills.ru');
+}
